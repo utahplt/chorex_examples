54ec6edcf402c6bb03800cee6d42cc98bda53d9b
Ashton Wiersdorf <mail@wiersdorf.dev>
2025-08-08 12:44:37 -0600

    Make TCP demo work

--- lib/mix/tasks/demo/tcp.ex
+++ lib/mix/tasks/demo/tcp.ex
@@ -3,8 +3,12 @@ defmodule Mix.Tasks.Demo.Tcp do
   @requirements ["loadpaths", "app.config", "app.start"]
 
   use Mix.Task
+  require Logger
 
   def run(_args) do
-    IO.puts("Hey look running the TCP service!")
+    Logger.info("Starting up the TCP handler...")
+    ChorexExamples.start_server()
+    Logger.info("Started; connect in another terminal window with `nc localhost 4242`")
+    IO.gets("Press 'ENTER' to quit")
   end
 end
